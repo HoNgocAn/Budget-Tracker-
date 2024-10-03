193e928747aa7306b4f5b583f9369e5e435ce2bd
HoNgocAn <anhongoc1306@gmail.com>
2024-10-03 08:45:48 +0700

    up bai

---
+++ budget_1/lib/screens/sign_up.dart
@@ -0,0 +1,121 @@
+import 'package:budget_tracker/screens/login.dart';
+import 'package:budget_tracker/utils/appvalidator.dart';
+import 'package:flutter/material.dart';
+
+class SignUpView extends StatelessWidget {
+  SignUpView({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(_formKey.currentContext!).showSnackBar(
+          const SnackBar(content: Text("Form submitted successfully")));
+    }
+  }
+
+  final appValidator = AppValidator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF252634),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+            key: _formKey,
+            child: Column(
+                children: [
+                const SizedBox(height: 80.0),
+            const SizedBox(
+              width: 250,
+              child: Text(
+                "Create New Account",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            const SizedBox(height: 50.0),
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: _buildInputDecoration("UserName", Icons.person),
+              validator: appValidator.validateUser,
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              keyboardType: TextInputType.emailAddress,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: _buildInputDecoration("Email", Icons.email),
+              validator: appValidator.validateEmail,
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              keyboardType: TextInputType.phone,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: _buildInputDecoration("PhoneNumber", Icons.call),
+              validator: appValidator.validatePhoneNumber,
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: _buildInputDecoration("Password", Icons.password),
+              validator: appValidator.validatePassword,
+              obscureText: true,
+            ),
+            const SizedBox(height: 40.0),
+            SizedBox(
+              height: 50,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: _submitForm,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF15900), // Màu nền của nút
+                  foregroundColor: Colors.white, // Màu chữ (foreground)
+                  textStyle:
+                  const TextStyle(fontSize: 24), // Kích thước chữ
+                ),
+                child: const Text("Create"),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context)=> LoginView()));
+              },
+              child: const Text(
+                "Login",
+                style: TextStyle(color: Color(0xFFF15900), fontSize: 25),
+              )),
+              ],
+            )),
+      ),
+    );
+  }
+
+  InputDecoration _buildInputDecoration(String label, IconData suffixIcon) {
+    return InputDecoration(
+        fillColor: const Color(0xAA494A59),
+        enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0x35949494))),
+        focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)),
+        filled: true,
+        labelStyle: const TextStyle(color: Color(0xFF949494)),
+        labelText: label,
+        suffixIcon: Icon(
+          suffixIcon,
+          color: const Color(0xFF949494),
+        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)));
+  }
+}
